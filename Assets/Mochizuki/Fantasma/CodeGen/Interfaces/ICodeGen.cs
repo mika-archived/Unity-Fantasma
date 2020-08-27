459a93d31ee5e35cff75056352c5d6210b699ae5
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-28 08:23:24 +0900

    feat: add base interface

---
+++ Assets/Mochizuki/Fantasma/CodeGen/Interfaces/ICodeGen.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace Mochizuki.Fantasma.CodeGen.Interfaces
+{
+    internal interface ICodeGen<out T> where T : CSharpSyntaxNode
+    {
+        T DeclarationToSyntax();
+    }
+}
