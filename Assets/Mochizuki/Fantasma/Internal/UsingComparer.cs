e7d114093fc3430bfe6f6f51e37a61e179b5f65e
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-28 08:06:05 +0900

    feat: add comparer for using directives

---
+++ Assets/Mochizuki/Fantasma/Internal/UsingComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mochizuki.Fantasma.Internal
+{
+    internal class UsingComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (string.IsNullOrWhiteSpace(x))
+                return 1;
+            if (string.IsNullOrWhiteSpace(y))
+                return 1;
+
+            if (x.StartsWith("System") && y.StartsWith("System"))
+                return string.Compare(x, y, StringComparison.Ordinal);
+            if (x.StartsWith("System") && !y.StartsWith("System"))
+                return -1;
+            if (!x.StartsWith("System") && y.StartsWith("System"))
+                return 1;
+            return string.Compare(x, y, StringComparison.Ordinal);
+        }
+    }
+}
