976bf9a74c9948c581fc7e7a50900f4227f3e836
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-29 01:09:53 +0900

    feat: add interfaces

---
+++ Assets/Mochizuki/Fantasma/CodeGen/Interfaces/IMemberDeclaration.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Mochizuki.Fantasma.CodeGen.Interfaces
+{
+    internal interface IMemberDeclaration<out T> : ICodeGen<T> where T : MemberDeclarationSyntax { }
+}
---
+++ Assets/Mochizuki/Fantasma/CodeGen/Interfaces/ITypeDeclaration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Mochizuki.Fantasma.CodeGen.Interfaces
+{
+    internal interface ITypeDeclaration<out T> : IMemberDeclaration<T> where T : BaseTypeDeclarationSyntax
+    {
+        /// <summary>
+        ///     the type declaration has nested class?
+        /// </summary>
+        bool HasNestedClass { get; }
+
+        /// <summary>
+        ///     direct nested types in this declaration
+        /// </summary>
+        ReadOnlyCollection<Type> NestedClasses { get; }
+
+        /// <summary>
+        ///     all type references in this declarations
+        /// </summary>
+        ReadOnlyCollection<Type> References { get; }
+
+        /// <summary>
+        ///     all alias type references in this declarations
+        /// </summary>
+        ReadOnlyCollection<Type> AliasReferences { get; }
+
+        /// <summary>
+        ///     add nested declarations to this declaration
+        /// </summary>
+        void AddDeclarations(params ITypeDeclaration<BaseTypeDeclarationSyntax>[] declarations);
+
+        /// <summary>
+        ///     extract class fields, methods, properties, references and base classes.
+        /// </summary>
+        void Extract();
+    }
+}
