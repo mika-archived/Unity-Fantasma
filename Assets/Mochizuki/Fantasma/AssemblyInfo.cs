f0ad550205ead71a46cb8c6ef4436427f920e22a
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-29 23:15:49 +0900

    chore: add AssemblyInfo for Mochizuki.Fantasma

---
+++ Assets/Mochizuki/Fantasma/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("Assets.Mochizuki.Fantasma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assets.Mochizuki.Fantasma")]
+[assembly: AssemblyCopyright("Copyright © 2020 Fuyuno Mikazuki / Natsuneko")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("70d67d9e-516e-41c6-847f-613e3efddbc1")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Mochizuki.Fantasma.Tests")]
+[assembly: InternalsVisibleTo("LINQPadQuery")]
