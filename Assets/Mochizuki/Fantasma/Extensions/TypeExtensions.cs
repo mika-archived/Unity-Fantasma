b41e612417cb55159de84f19c2a4f9dae093bc21
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-30 16:30:53 +0900

    feat: add extension class for type

---
+++ Assets/Mochizuki/Fantasma/Extensions/TypeExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mochizuki.Fantasma.Extensions
+{
+    internal static class TypeExtensions
+    {
+
+        public static string FullNameWithoutNamespace(this Type t)
+        {
+            return t.DeclaringType == null ? t.Name : $"{FullNameWithoutNamespace(t.DeclaringType)}.{t.Name}";
+        }
+
+        public static string KeywordNormalizedName(this Type type)
+        {
+            switch (type)
+            {
+                // ReSharper disable PatternAlwaysOfType
+
+                case Type _ when type == typeof(bool):
+                    return "bool";
+
+                case Type _ when type == typeof(byte):
+                    return "byte";
+
+                case Type _ when type == typeof(sbyte):
+                    return "sbyte";
+
+                case Type _ when type == typeof(char):
+                    return "char";
+
+                case Type _ when type == typeof(decimal):
+                    return "decimal";
+
+                case Type _ when type == typeof(double):
+                    return "double";
+
+                case Type _ when type == typeof(float):
+                    return "float";
+
+                case Type _ when type == typeof(int):
+                    return "int";
+
+                case Type _ when type == typeof(uint):
+                    return "uint";
+
+                case Type _ when type == typeof(long):
+                    return "long";
+
+                case Type _ when type == typeof(ulong):
+                    return "ulong";
+
+                case Type _ when type == typeof(short):
+                    return "short";
+
+                case Type _ when type == typeof(ushort):
+                    return "ushort";
+
+                case Type _ when type == typeof(object):
+                    return "object";
+
+                case Type _ when type == typeof(string):
+                    return "string";
+
+                case Type _ when type == typeof(void):
+                    return "void";
+
+                default:
+                    return type.FullNameWithoutNamespace();
+
+                // ReSharper restore PatternAlwaysOfType
+            }
+        }
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/Extensions/TypeExtensionsTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using Mochizuki.Fantasma.Extensions;
+
+using NUnit.Framework;
+
+using Object = UnityEngine.Object;
+
+namespace Mochizuki.Fantasma.Tests.Extensions
+{
+    [TestFixture]
+    internal class TypeExtensionsTest
+    {
+        // for test case
+        private static class A
+        {
+            public class B { }
+        }
+
+        [Test]
+        [TestCase(typeof(bool), "Boolean")]
+        [TestCase(typeof(TypeExtensionsTest), "TypeExtensionsTest")]
+        [TestCase(typeof(A.B), "TypeExtensionsTest.A.B")]
+        public void FullnameWithoutNamespaceTest(Type t, string expected)
+        {
+            Assert.AreEqual(expected, t.FullNameWithoutNamespace());
+        }
+
+        [Test]
+        [TestCase(typeof(bool), true)]
+        [TestCase(typeof(byte), true)]
+        [TestCase(typeof(sbyte), true)]
+        [TestCase(typeof(char), true)]
+        [TestCase(typeof(decimal), true)]
+        [TestCase(typeof(double), true)]
+        [TestCase(typeof(float), true)]
+        [TestCase(typeof(int), true)]
+        [TestCase(typeof(uint), true)]
+        [TestCase(typeof(long), true)]
+        [TestCase(typeof(ulong), true)]
+        [TestCase(typeof(short), true)]
+        [TestCase(typeof(ushort), true)]
+        [TestCase(typeof(object), true)]
+        [TestCase(typeof(string), true)]
+        [TestCase(typeof(void), true)]
+        [TestCase(typeof(Object), false)]
+        public void IsKeywordTypeTest(Type t, bool expected)
+        {
+            Assert.AreEqual(expected, t.IsKeywordType());
+        }
+
+        [Test]
+        [TestCase(typeof(bool), "bool")]
+        [TestCase(typeof(byte), "byte")]
+        [TestCase(typeof(sbyte), "sbyte")]
+        [TestCase(typeof(char), "char")]
+        [TestCase(typeof(decimal), "decimal")]
+        [TestCase(typeof(double), "double")]
+        [TestCase(typeof(float), "float")]
+        [TestCase(typeof(int), "int")]
+        [TestCase(typeof(uint), "uint")]
+        [TestCase(typeof(long), "long")]
+        [TestCase(typeof(ulong), "ulong")]
+        [TestCase(typeof(short), "short")]
+        [TestCase(typeof(ushort), "ushort")]
+        [TestCase(typeof(object), "object")]
+        [TestCase(typeof(string), "string")]
+        [TestCase(typeof(void), "void")]
+        [TestCase(typeof(Object), "Object")]
+        public void KeywordNormalizedNameTest(Type t, string expected)
+        {
+            Assert.AreEqual(expected, t.KeywordNormalizedName());
+        }
+    }
+}
