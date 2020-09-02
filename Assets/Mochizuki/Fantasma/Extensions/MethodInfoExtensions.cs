5a191e4a9fd4109b4098521ecb1c75df74f96891
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-02 20:12:00 +0900

    feat: add method that check method is override

---
+++ Assets/Mochizuki/Fantasma/Extensions/MethodInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Mochizuki.Fantasma.Extensions
+{
+    internal static class MethodInfoExtensions
+    {
+        public static bool IsOverride(this MethodInfo obj)
+        {
+            return obj.GetBaseDefinition().DeclaringType != obj.DeclaringType;
+        }
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/Extensions/MethodInfoExtensionsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Mochizuki.Fantasma.Extensions;
+
+using NUnit.Framework;
+
+namespace Mochizuki.Fantasma.Tests.Extensions
+{
+    [TestFixture]
+    internal class MethodInfoExtensionsTest
+    {
+        public interface IInterfaceA
+        {
+            void MethodA();
+        }
+
+        public class InterfaceImplClassA : IInterfaceA
+        {
+            public void MethodA() { }
+        }
+
+        public abstract class BaseClassA
+        {
+            public abstract void MethodA();
+        }
+
+        public class AbstractImplClassA : BaseClassA
+        {
+            public override void MethodA() { }
+        }
+
+        public class ClassA
+        {
+            public virtual void MethodA() { }
+        }
+
+        public class InheritClassA : ClassA
+        {
+            public override void MethodA() { }
+        }
+
+        [Test]
+        [TestCase(typeof(InterfaceImplClassA), "MethodA", false)]
+        [TestCase(typeof(AbstractImplClassA), "MethodA", true)]
+        [TestCase(typeof(ClassA), "MethodA", false)]
+        [TestCase(typeof(InheritClassA), "MethodA", true)]
+        public void IsOverrideTest(Type t, string m, bool expected)
+        {
+            var method = t.GetMethod(m);
+            Assert.AreEqual(expected, method.IsOverride());
+        }
+    }
+}
