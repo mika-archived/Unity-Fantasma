cff0b0fc083d2d24065bcfb194de5fe6a5884695
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-02 20:03:15 +0900

    feat: support namespace

---
+++ Assets/Mochizuki/Fantasma/CodeGen/Namespace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Mochizuki.Fantasma.CodeGen.Interfaces;
+
+namespace Mochizuki.Fantasma.CodeGen
+{
+    internal class Namespace : IMemberDeclaration<NamespaceDeclarationSyntax>
+    {
+        private readonly Type _type;
+
+        public bool HasNamespace => !string.IsNullOrEmpty(_type.Namespace);
+
+        public Namespace(Type type)
+        {
+            _type = type;
+        }
+
+        public NamespaceDeclarationSyntax DeclarationToSyntax(bool implementation)
+        {
+            return SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(_type.Namespace));
+        }
+
+        public ReadOnlyCollection<Type> References => new ReadOnlyCollection<Type>(Array.Empty<Type>());
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/CodeGen/NamespaceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+
+using Mochizuki.Fantasma.CodeGen;
+
+using NUnit.Framework;
+
+using UnityEngine;
+
+namespace Mochizuki.Fantasma.Tests.CodeGen
+{
+    [TestFixture]
+    internal class NamespaceTest
+    {
+        public class AClass { }
+
+        [Test]
+        [TestCase(typeof(GameObject), "namespace UnityEngine{}")]
+        [TestCase(typeof(AClass), "namespace Mochizuki.Fantasma.Tests.CodeGen{}")]
+        public void DeclarationToSyntaxTest(Type t, string expected)
+        {
+            Assert.AreEqual(expected, new Namespace(t).DeclarationToSyntax(true).NormalizeWhitespace().ToFullString().Replace(Environment.NewLine, ""));
+        }
+    }
+}
