8394444e4cb514d273273021d7dc1e1106022c21
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-02 12:10:44 +0900

    feat: suppor property

---
+++ Assets/Mochizuki/Fantasma/CodeGen/Property.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Mochizuki.Fantasma.CodeGen.Interfaces;
+using Mochizuki.Fantasma.Extensions;
+
+namespace Mochizuki.Fantasma.CodeGen
+{
+    internal class Property : IMemberDeclaration<PropertyDeclarationSyntax>
+    {
+        private readonly PropertyInfo _property;
+
+        public Type Type => _property.PropertyType;
+
+        public Property(PropertyInfo property)
+        {
+            _property = property;
+        }
+
+        public PropertyDeclarationSyntax DeclarationToSyntax()
+        {
+            var modifiers = new List<SyntaxKind> { SyntaxKind.PublicKeyword };
+            if (_property.GetAccessors().Any(w => w.IsStatic))
+                modifiers.Add(SyntaxKind.StaticKeyword);
+
+            var property = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName(Type.NormalizedName()), _property.Name).AddModifiers(modifiers.Select(SyntaxFactory.Token).ToArray());
+            var accessors = new List<AccessorDeclarationSyntax>();
+            if (_property.CanRead && _property.GetMethod.IsPublic)
+                accessors.Add(SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration));
+            if (_property.CanWrite && _property.SetMethod.IsPublic)
+                accessors.Add(SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration));
+
+            return property.AddAccessorListAccessors(accessors.Select(w => w.WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))).ToArray());
+        }
+
+        public ReadOnlyCollection<Type> References => new ReadOnlyCollection<Type>(new List<Type> { Type });
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/CodeGen/PropertyTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+
+using Mochizuki.Fantasma.CodeGen;
+
+using NUnit.Framework;
+
+using UnityEngine;
+
+namespace Mochizuki.Fantasma.Tests.CodeGen
+{
+    [TestFixture]
+    internal class PropertyTest
+    {
+        private class AClass<T>
+        {
+            // ReSharper disable UnusedMember.Local
+            // ReSharper disable UnassignedGetOnlyAutoProperty
+
+            public int PropertyA { get; }
+
+            public GameObject PropertyB { get; private set; }
+
+            public int PropertyC { private get; set; }
+
+            public T PropertyD { get; set; }
+
+            #region PropertyE
+
+            private string _propertyE;
+
+            public string PropertyE
+            {
+                get => _propertyE;
+                set
+                {
+                    if (string.Equals(_propertyE, value, StringComparison.Ordinal))
+                        return;
+                    _propertyE = value;
+                }
+            }
+
+            #endregion
+
+            // ReSharper disable once StaticMemberInGenericType
+            public static double PropertyF { get; private set; }
+
+            // ReSharper restore UnusedMember.Local
+            // ReSharper restore UnassignedGetOnlyAutoProperty
+        }
+
+        [Test]
+        [TestCase(typeof(AClass<>), 0, "public int PropertyA{get;}")]
+        [TestCase(typeof(AClass<>), 1, "public GameObject PropertyB{get;}")]
+        [TestCase(typeof(AClass<>), 2, "public int PropertyC{set;}")]
+        [TestCase(typeof(AClass<>), 3, "public T PropertyD{get;set;}")]
+        [TestCase(typeof(AClass<>), 4, "public string PropertyE{get;set;}")]
+        [TestCase(typeof(AClass<>), 5, "public static double PropertyF{get;}")]
+        public void DeclarationToSyntax(Type cls, int idx, string expected)
+        {
+            var property = cls.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)[idx];
+            Assert.AreEqual(expected, new Property(property).DeclarationToSyntax().NormalizeWhitespace().ToFullString().Replace(Environment.NewLine, "").Replace("    ", ""), expected);
+        }
+    }
+}
