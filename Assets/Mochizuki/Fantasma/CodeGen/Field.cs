5a95028b92fa42faf8cfe58f8a39d64819046a31
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-05 18:41:34 +0900

    feat: support field

---
+++ Assets/Mochizuki/Fantasma/CodeGen/Field.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Mochizuki.Fantasma.CodeGen.Interfaces;
+using Mochizuki.Fantasma.Extensions;
+
+namespace Mochizuki.Fantasma.CodeGen
+{
+    internal class Field : IMemberDeclaration<FieldDeclarationSyntax>
+    {
+        private readonly FieldInfo _field;
+
+        public Type Type => _field.FieldType;
+
+        public string Name => _field.Name;
+
+        public Field(FieldInfo field)
+        {
+            _field = field;
+        }
+
+        public FieldDeclarationSyntax DeclarationToSyntax(bool implementation)
+        {
+            var variable = SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName(Type.NormalizedName())).AddVariables(SyntaxFactory.VariableDeclarator(Name));
+            var modifiers = new List<SyntaxKind> { SyntaxKind.PublicKeyword };
+            if (_field.IsStatic && !_field.IsLiteral)
+                modifiers.Add(SyntaxKind.StaticKeyword);
+            if (_field.IsInitOnly)
+                modifiers.Add(SyntaxKind.ReadOnlyKeyword);
+            if (_field.IsLiteral)
+            {
+                modifiers.Add(SyntaxKind.ConstKeyword);
+
+                var equals = SyntaxFactory.EqualsValueClause(SyntaxFactory.Token(SyntaxKind.EqualsToken), CreateInitializerSyntax());
+                variable = variable.WithVariables(SyntaxFactory.SeparatedList(new List<VariableDeclaratorSyntax> { variable.Variables.First().WithInitializer(equals) }));
+            }
+
+            return SyntaxFactory.FieldDeclaration(variable).AddModifiers(modifiers.Select(SyntaxFactory.Token).ToArray());
+        }
+
+        public ReadOnlyCollection<Type> References => new ReadOnlyCollection<Type>(new List<Type> { Type });
+
+        private ExpressionSyntax CreateInitializerSyntax()
+        {
+            if (Type == typeof(string))
+                return SyntaxFactory.ParseExpression($"\"{_field.GetValue(null)}\"");
+            if (Type.IsEnum)
+                return SyntaxFactory.ParseExpression($"{Type.NormalizedName()}.{_field.GetValue(null)}");
+            return SyntaxFactory.ParseExpression(_field.GetValue(null).ToString());
+        }
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/CodeGen/FieldTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+
+using Mochizuki.Fantasma.CodeGen;
+
+using NUnit.Framework;
+
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+// ReSharper disable ClassNeverInstantiated.Local
+
+#pragma warning disable 649
+#pragma warning disable RCS1102 // Make class static.
+
+namespace Mochizuki.Fantasma.Tests.CodeGen
+{
+    [TestFixture]
+    internal class FieldTest
+    {
+        private class ClassA
+        {
+            // ReSharper disable once UnusedMember.Local
+            public const string FieldC = "Hello, World";
+
+            // ReSharper disable once UnusedMember.Local
+            public const long FieldG = long.MaxValue;
+
+            public const DayOfWeek FieldH = DayOfWeek.Friday;
+
+            public static long FieldD;
+            public static readonly GameObject FieldE;
+            public readonly List<GameObject> FieldB;
+            public string FieldA;
+            public ClassC<string>.ClassD FieldF;
+
+            // ReSharper disable once UnusedTypeParameter
+            public class ClassC<T>
+            {
+                public class ClassD
+                {
+                    // ReSharper disable once UnusedTypeParameter
+                    public class ClassE<T1> { }
+                }
+            }
+        }
+
+        // ReSharper disable once UnusedTypeParameter
+        private class ClassB<T>
+        {
+            public uint FieldA;
+            public ClassA.ClassC<IEnumerable<KeyValuePair<string, object>>>.ClassD FieldB;
+            public ClassA.ClassC<IEnumerable<KeyValuePair<string, Object>>>.ClassD.ClassE<List<string>> FieldC;
+        }
+
+        [Test]
+        [TestCase(typeof(ClassA), "FieldA", "public string FieldA;")]
+        [TestCase(typeof(ClassA), "FieldB", "public readonly List<GameObject> FieldB;")]
+        [TestCase(typeof(ClassA), "FieldC", "public const string FieldC = \"Hello, World\";")]
+        [TestCase(typeof(ClassA), "FieldD", "public static long FieldD;")]
+        [TestCase(typeof(ClassA), "FieldE", "public static readonly GameObject FieldE;")]
+        [TestCase(typeof(ClassA), "FieldF", "public FieldTest.ClassA.ClassC<string>.ClassD FieldF;")]
+        [TestCase(typeof(ClassA), "FieldG", "public const long FieldG = 9223372036854775807;")]
+        [TestCase(typeof(ClassA), "FieldH", "public const DayOfWeek FieldH = DayOfWeek.Friday;")]
+        [TestCase(typeof(ClassB<>), "FieldA", "public uint FieldA;")]
+        [TestCase(typeof(ClassB<>), "FieldB", "public FieldTest.ClassA.ClassC<IEnumerable<KeyValuePair<string, object>>>.ClassD FieldB;")]
+        [TestCase(typeof(ClassB<>), "FieldC", "public FieldTest.ClassA.ClassC<IEnumerable<KeyValuePair<string, Object>>>.ClassD.ClassE<List<string>> FieldC;")]
+        public void DeclarationToSyntax(Type cls, string fld, string expected)
+        {
+            var field = cls.GetField(fld, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
+            Assert.AreEqual(expected, new Field(field).DeclarationToSyntax(true).NormalizeWhitespace().ToFullString());
+        }
+    }
+}
