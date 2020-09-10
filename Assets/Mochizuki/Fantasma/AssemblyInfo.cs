00ed1f1db8c9f4710901339be2d347eeed42a775
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-10 18:26:31 +0900

    fix: remove `Assets` prefix

--- Assets/Mochizuki/Fantasma/AssemblyInfo.cs
+++ Assets/Mochizuki/Fantasma/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Assets.Mochizuki.Fantasma")]
+[assembly: AssemblyTitle("Mochizuki.Fantasma")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assets.Mochizuki.Fantasma")]
+[assembly: AssemblyProduct("Mochizuki.Fantasma")]
 [assembly: AssemblyCopyright("Copyright © 2020 Fuyuno Mikazuki / Natsuneko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
