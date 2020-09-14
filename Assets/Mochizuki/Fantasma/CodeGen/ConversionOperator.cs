6ef08b8acf8172926f887239139d17808b90f097
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-14 22:14:10 +0900

    feat: support conversion-operator overloadings

---
+++ Assets/Mochizuki/Fantasma/CodeGen/ConversionOperator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Mochizuki.Fantasma.CodeGen.Interfaces;
+using Mochizuki.Fantasma.Extensions;
+
+namespace Mochizuki.Fantasma.CodeGen
+{
+    internal class ConversionOperator : IMemberDeclaration<ConversionOperatorDeclarationSyntax>
+    {
+        private static readonly ReadOnlyDictionary<string, SyntaxKind> MethodOperatorMap = new ReadOnlyDictionary<string, SyntaxKind>(new Dictionary<string, SyntaxKind>
+        {
+            { "op_Implicit", SyntaxKind.ImplicitKeyword },
+            { "op_Explicit", SyntaxKind.ExplicitKeyword }
+        });
+
+        private readonly MethodInfo _overload;
+
+        public ConversionOperator(MethodInfo overload)
+        {
+            _overload = overload;
+        }
+
+        public ConversionOperatorDeclarationSyntax DeclarationToSyntax(bool implementation)
+        {
+            var modifiers = new List<SyntaxKind> { SyntaxKind.PublicKeyword, SyntaxKind.StaticKeyword };
+            var statements = SyntaxFactory.Block(SyntaxFactory.ParseStatement("return default;"));
+            var parameters = _overload.GetParameters().Select(w => SyntaxFactory.Parameter(SyntaxFactory.Identifier(w.Name)).WithType(SyntaxFactory.ParseTypeName(w.ParameterType.NormalizedName())));
+
+            return SyntaxFactory.ConversionOperatorDeclaration(SyntaxFactory.Token(MethodOperatorMap[_overload.Name]), SyntaxFactory.ParseTypeName(_overload.ReturnType.NormalizedName()))
+                                .AddModifiers(modifiers.Select(SyntaxFactory.Token).ToArray())
+                                .AddParameterListParameters(parameters.ToArray())
+                                .WithBody(statements);
+        }
+
+        public ReadOnlyCollection<Type> References { get; }
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/CodeGen/ConversionOperatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+
+using Mochizuki.Fantasma.CodeGen;
+
+using NUnit.Framework;
+
+namespace Mochizuki.Fantasma.Tests.CodeGen
+{
+    [TestFixture]
+    internal class ConversionOperatorTest
+    {
+        // IntWrapper works as Int32
+        private class IntWrapper
+        {
+            private readonly int _value;
+
+            public IntWrapper(int value)
+            {
+                _value = value;
+            }
+
+            #region Overloads
+
+            // operator overloads
+            // reference: https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/operators/operator-overloading
+
+            // user-defined conversion
+            public static implicit operator bool(IntWrapper a)
+            {
+                return a._value != 0;
+            }
+
+            public static explicit operator IntWrapper(bool a)
+            {
+                return new IntWrapper(a ? 1 : 0);
+            }
+
+            #endregion
+        }
+
+        [Test]
+        [TestCase(typeof(IntWrapper), "op_Implicit", "public static implicit operator bool (ConversionOperatorTest.IntWrapper a){    return default;}")]
+        [TestCase(typeof(IntWrapper), "op_Explicit", "public static explicit operator ConversionOperatorTest.IntWrapper(bool a){    return default;}")]
+        public void DeclarationToSyntax(Type cls, string @operator, string expected)
+        {
+            var overload = cls.GetMethod(@operator, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
+            Assert.AreEqual(expected, new ConversionOperator(overload).DeclarationToSyntax(true).NormalizeWhitespace().ToFullString().Replace(Environment.NewLine, ""));
+        }
+    }
+}
