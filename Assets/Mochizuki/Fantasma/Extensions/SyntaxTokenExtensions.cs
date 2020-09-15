5939d5336f1ff46f6fb366bb44774de710f0e81a
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-09-16 08:32:10 +0900

    feat: support keyword identifier

---
+++ Assets/Mochizuki/Fantasma/Extensions/SyntaxTokenExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Mochizuki.Fantasma.Extensions
+{
+    internal static class SyntaxTokenExtensions
+    {
+        public static SyntaxToken Normalize(this SyntaxToken token)
+        {
+            var keywords = SyntaxFacts.GetKeywordKinds().Select(SyntaxFactory.Token).Select(w => w.ToFullString());
+            return keywords.Contains(token.ToFullString()) ? SyntaxFactory.ParseToken($"@{token.ToFullString()}") : token;
+        }
+    }
+}
---
+++ Assets/Mochizuki/Fantasma/Tests/Extensions/SyntaxTokenExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+using Mochizuki.Fantasma.Extensions;
+
+using NUnit.Framework;
+
+namespace Mochizuki.Fantasma.Tests.Extensions
+{
+    [TestFixture]
+    internal class SyntaxTokenExtensionsTest
+    {
+        [Test]
+        [TestCase("hello", "hello")]
+        [TestCase("class", "@class")]
+        public void Normalize(string identifier, string expected)
+        {
+            Assert.AreEqual(SyntaxFactory.Identifier(identifier).Normalize().ToFullString(), expected);
+        }
+    }
+}
